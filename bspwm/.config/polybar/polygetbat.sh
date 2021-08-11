7ddc2f65b3e111e7ccfe61ebfe0e6912c2bd0ebf
Guzman Zugnoni <gooseiman@protonmail.com>
2021-08-11 17:59:24 -0300

    Tidy up battery script

--- bspwm/.config/polybar/polygetbat.sh
+++ bspwm/.config/polybar/polygetbat.sh
@@ -2,31 +2,31 @@
 
 bat_number=0
 acpi=$(acpi -b 2>/dev/null| grep "Battery $bat_number")
+[ "$acpi" = "" ] && exit 1
 ac_adapt=$(acpi -a | awk '{print $2}')
 mode=$(echo "$acpi" | awk '{print $3}')
 percent=$(echo "$acpi" | awk '{print $4}'| grep -Eo '[0-9]*')
-remaining=$(echo "$acpi" | awk '{print $5}')
 symbol=""
 
 
 if [ "$mode" = "Discharging," ]; then
-  symbol='⚡ '
+  symbol='⚡'
 elif [ "$mode" = "Charging," ]; then
-  symbol='🔌 '
+  symbol='🔌'
 elif [ "$mode" = "Unknown," ]; then
   if [ "$ac_adapt" = "on-line," ]; then
-    symbol='⚡ '
+    symbol='⚡'
   elif [ "$ac_adapt" = "off-line," ]; then
-    symbol='🔌 '
+    symbol='🔌'
   fi
 else
-  symbol='🔋 '
+  symbol='🔋'
 fi
-
-printf "%s%s" "$symbol" "$percent%"
 
 if [ "$mode" = "Discharging," ] || [ "$mode" = "Charging," ] || [ "$ac_adapt" = "off-line," ]; then
-  echo " ($remaining)"
+	remaining=$(echo "$acpi" | awk '{print $5}')
+	remaining="($remaining)"
 fi
 
+printf "%s %s %s" "$symbol" "$percent%" "$remaining"
 
