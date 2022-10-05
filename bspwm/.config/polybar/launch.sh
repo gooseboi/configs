6c925e4938d08ed94b2cf5d99593b20a532a28b4
Guzman Zugnoni <gooseiman@protonmail.com>
2022-10-05 09:21:00 -0300

    Fix polybar launch script to properly kill the previous session

--- bspwm/.config/polybar/launch.sh
+++ bspwm/.config/polybar/launch.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 
 # Terminate already running bar instances
-killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# and wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do
+	killall -q -KILL polybar
+	sleep 1
+done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
