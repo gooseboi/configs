7f63b4e57548f0eef3a32e62c54dda1f7131e226
Guzman Zugnoni <gooseiman@protonmail.com>
2023-06-16 14:45:42 -0300

    change shortcut for project files

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ps', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>gs', builtin.live_grep, {})
 
 -- Make sure telescope is installed
