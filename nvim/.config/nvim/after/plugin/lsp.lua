0f1ba30feb62f8646750da9ab8289676337aec59
Guzman Zugnoni <gooseiman@protonmail.com>
2024-04-19 12:12:12 -0300

    format code

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,28 +1,31 @@
+--- @param bufnr integer
 local on_attach = function(_, bufnr)
+	--- @param keys string
+	--- @param func function
+	local bufmap = function(keys, func)
+		vim.keymap.set('n', keys, func, { buffer = bufnr })
+	end
 
-  local bufmap = function(keys, func)
-    vim.keymap.set('n', keys, func, { buffer = bufnr })
-  end
+	bufmap('<leader>rn', vim.lsp.buf.rename)
+	bufmap('<leader>.', vim.lsp.buf.code_action)
 
-  bufmap('<leader>rn', vim.lsp.buf.rename)
-  bufmap('<leader>.', vim.lsp.buf.code_action)
+	bufmap('gd', vim.lsp.buf.definition)
+	bufmap('gD', vim.lsp.buf.declaration)
+	bufmap('gI', vim.lsp.buf.implementation)
+	bufmap('<leader>D', vim.lsp.buf.type_definition)
 
-  bufmap('gd', vim.lsp.buf.definition) bufmap('gD', vim.lsp.buf.declaration)
-  bufmap('gI', vim.lsp.buf.implementation)
-  bufmap('<leader>D', vim.lsp.buf.type_definition)
+	bufmap('gr', require('telescope.builtin').lsp_references)
+	bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
+	bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
 
-  bufmap('gr', require('telescope.builtin').lsp_references)
-  bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
-  bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+	bufmap("[d", vim.diagnostic.goto_prev)
+	bufmap("]d", vim.diagnostic.goto_next)
 
-  bufmap("[d", vim.diagnostic.goto_prev)
-  bufmap("]d", vim.diagnostic.goto_next)
+	bufmap('K', vim.lsp.buf.hover)
 
-  bufmap('K', vim.lsp.buf.hover)
-
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, {})
+	vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+		vim.lsp.buf.format()
+	end, {})
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -35,6 +38,7 @@ require("mason-lspconfig").setup({
 		"rust_analyzer",
 		"gopls",
 		"nil_ls",
+		"bashls",
 	},
 
 	handlers = {
@@ -71,6 +75,6 @@ require("mason-lspconfig").setup({
 					}
 				}
 			}
-		end
+		end,
 	}
 })
